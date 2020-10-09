986837302056c057b5749856262622ec92ada136
Auekun <614259031@webmail.npru.ac.th>
2020-10-09 23:15:58 +0700

    mystagram

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// @import express-handlebars
+
 const PORT = process.env.PORT || 8080;
 const express = require('express');
 const hbs = require('express-handlebars');
@@ -8,9 +8,7 @@ const path = require('path');
 app.engine('hbs', hbs({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-// app.get('/',(req,res)=>{
-//     res.send("<h1>My Portfolio</h1>");
-// });
+
 app.use('/', router);
 app.listen(
     PORT,
--- public/script.js
+++ public/script.js
@@ -1,39 +1,40 @@
-function removeAllPhoto() { //funtion ลบรูปเก่า หรือเคลียค่า
-    const galleryElement = document.querySelector(".gallery");
-    galleryElement.innerHTML = ''; //ทำให้ค่าข้างในเป็นว่าง
-}
 
-function addImg(keyword, index) {
-    const img = document.createElement('img');
-    img.src = `https://source.unsplash.com/400x225?${keyword}&sig=${index}`;
+
+function addImage(keyword,index){
+    const image = document.createElement('img');
+    image.src =`https://source.unsplash.com/400x225?${keyword}&sig=${index}`;
 
     const gallery = document.querySelector(".gallery");
+    gallery.appendChild(image)
 
-
-    gallery.appendChild(img);
+} 
+function removeAllPhoto(){
+    const galleryElement = document.querySelector(".gallery");
+    galleryElement.innerHTML = '';
 }
 
-function searchPhoto(event) {
+
+function searchPhoto(event){
     const keyword = event.target.value;
-    removeAllPhoto();
-    if (event.key === 'Enter' && keyword)
-        for (let i = 0; i < 9; i++) {
-            addImg(keyword, i);
+    if(event.key === "Enter" && keyword){
+        removeAllPhoto();
+        for(let i=0;i<9;i++){
+            addImage(keyword,i);
         }
+    }
 }
 
 function handleUpdate() {
-    const suffix = this.dataset.sizing || '';
-    console.log(`--${this.name}:${this.value+suffix}`);
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    const suffix = this.dataset.sizing ||'';
+    document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
 }
-
-function main() {
-    const inputContorls = document.querySelectorAll('.controls input');
-    inputContorls.forEach((input) => { input.addEventListener('change', handleUpdate); });
-    // inputContorls.forEach((input) => { input.addEventListener('mousemove', handleUpdate); });
-
-    const inputElement = document.querySelector('.search');
-    inputElement.addEventListener('keydown', searchPhoto);
+function main(){
+        const inputControl = document.querySelectorAll('.controls input');
+        inputControl.forEach((input) => {input.addEventListener('change',handleUpdate);});
+        inputControl.forEach((input) => {input.addEventListener('mousemove',handleUpdate);});
+        
+        const inputElement = document.querySelector('.search');
+        inputElement.addEventListener('keydown',searchPhoto);
+    
 }
 main();
